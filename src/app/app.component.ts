4dda0875a89146f71c94c6475e4d18147a07110f
Thomas <t.girault@gmail.com>
2020-01-18 15:27:51 +0100

    Création de la structure de la page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
+export interface Language {
+  value: string;
+  viewValue: string;
+}
+
+export interface Menu {
+  label: string;
+  path: string;
+}
+
+/**
+ * Composant principal.
+ * @author Thomas Girault
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'my-title';
-  footer = 'my-footer';
+export class AppComponent implements OnDestroy {
+
+  languages: Language[] = [
+    { value: 'fr', viewValue: 'Français' },
+    { value: 'en', viewValue: 'English' }
+  ];
+
+  menus: Menu[] = [
+    { label: 'Accueil', path: '/home' },
+    { label: 'Réservation', path: '/booking' },
+    { label: 'Contact', path: '/contact' },
+  ]
+
+  language: string;
+
+  mobileQuery: MediaQueryList;
+
+  content = 'Mon texte';
+
+  private _mobileQueryListener: () => void;
+
+  /**
+   * Constructeur du composant.
+   * @param translate service de gestion des traductions
+   */
+  constructor(private translate: TranslateService, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    translate.setDefaultLang('fr');
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  useLanguage() {
+    this.translate.use(this.language);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { MatIconModule, MatInputModule, MatListModule, MatSelectModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
+import { MatCommonModule, MatOptionModule } from '@angular/material/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,8 +24,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         deps: [HttpClient],
       },
     }),
+    FormsModule,
+    MatCommonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatInputModule,
+    MatSelectModule,
+    MatOptionModule
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
